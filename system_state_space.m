e4575036d3d47cbef5c5b3978b51ff1b6a32bd04
Jason Moore <moorepants@gmail.com>
2012-04-22 22:19:00 -0700

    Added the option of returning the lateral deviation or heading tracking closed loop system state space.

--- system_state_space.m
+++ system_state_space.m
@@ -1,14 +1,22 @@
-function sys = system_state_space(bicycle, gains, neuro, inputs, outputs)
-% function sys = system_state_space(bicycle, gains, neuro, inputs, outputs)
+function sys = system_state_space(stype, bicycle, gains, neuro, inputs, outputs)
+% function sys = system_state_space(stype, bicycle, gains, neuro, inputs, outputs)
 %
-% Returns the full system state space of the Hess bicycle-rider system with
-% bicycle steer torque as the controlled input.
+% Returns the closed loop system state space of the Hess bicycle-rider
+% system for heading or lateral deviation tracking with bicycle steer torque
+% as the controlled input.
 %
-% bicycle : structure
+% stype : string
+%   This should specify 'lateral' or 'heading' for lateral deviation
+%   tracking and heading tracking closed loop systems, respectively.
+% bicycle : ss
+%   A continuous state space system describing the bicycle dynamics. The
+%   following parameters must be set.
 %   A : matrix, m x m
 %       The state matrix of the bicycle system. The minimum set of states
-%       must be yP, psi, phi, delta, phiDot, deltaDot to allow for the
-%       calculation of the necessary output variables.
+%       for the 'lateral' system type must be [yP, psi, phi, delta, phiDot,
+%       deltaDot] and the minimum set of states for the 'heading' type must
+%       be [psi, phi, delta, phiDot, deltaDot] to allow for the calculation
+%       of the necessary output variables.
 %   B : matrix, m X n
 %       The input matrix of the bicycle system. The input must at least
 %       include `tDelta`.
@@ -17,59 +25,71 @@ function sys = system_state_space(bicycle, gains, neuro, inputs, outputs)
 %       least yQ, psi, phi, phiDot, and delta.
 %   D : matrix, o X n
 %       The feed forward matrix of the bicycle system. This is always zero.
-%   states : cell array of chars, m x 1
+%   StateName : cell array of chars, m x 1
 %       The names of the states in order.
-%   inputs : cell array of chars, n x 1
+%   InputName : cell array of chars, n x 1
 %       The names of the inputs in order.
-%   outputs : cell array of chars, o x 1
+%   OutputName : cell array of chars, o x 1
 %       The names of the outputs in order.
-% gains : matrix, 1 x 5
-%   The feedback gains [kDelta, kPhiDot, kPhi, kPsi, kY].
-% neuro : double
-%   The neuromuscular frequency.
+% gains : matrix, 1 x 5 or 1 x 4
+%   The feedback gains [kDelta, kPhiDot, kPhi, kPsi, kYq]. kYq will be
+%   ignored if provided for the 'heading' system type.
+% neuro : double, 1 x 2
+%   The neuromuscular frequency and damping ratio, [wnm, zetanm].
 % inputs : cell array of chars, m + 2 x q
-%   The names of the desired system inputs in order. If the array is empty,
-%   then the system B matrix is empty.
-% ouputs : cell array of chars, p x m + 2
+%   The names of the desired q system external inputs in order. If the array
+%   is empty, then the system B matrix is empty. 'yQc' is an option for the
+%   lateral system and 'yPsic' for the heading system type for lateral
+%   deviation and reference tracking inputs.
+% outputs : cell array of chars, p x m + 2
 %   The names of the desired system outputs in order. These can be a subset
 %   of the outputs in the bicycle outputs in addition to `tDelta` and
 %   `tDeltaDot`.
 %
 % Returns
 % -------
-% sys : structure
-%   states : cell array of chars, m + 2 x 1
+% sys : ss
+%   The closed loop 'lateral' or 'heading' state space system.
+%   StateName : cell array of chars, m + 2 x 1
 %       The names of the desired system states in order. This will always be
 %       the states for the bicycle plus two additional states `tDelta` and
 %       `tDeltaDot`.
-%   inputs : cell array of chars, n x 1
+%   InputName : cell array of chars, n x 1
 %       The names of the desired system inputs in order. These will always
 %       be the inputs of the bicycle minus `tDelta` and plus `yc`, the
 %       commanded lateral deviation of the front wheel.
 
 % build the state matrix
 % append the two additional state names
-states = [bicycle.states, 'tDelta', 'tDeltaDot'];
+states = [bicycle.StateName; 'tDelta'; 'tDeltaDot'];
 % add room for the two new states, tDelta and tDeltaDot
 [mA, nA] = size(bicycle.A);
 A = zeros(mA + 2, nA + 2);
 % put the bicycle equations in the upper left corner
 A(1:mA, 1:nA) = bicycle.A;
 % put the steer torque B column in the new A matrix
-A(1:mA, index('tDelta', states)) = bicycle.B(:, index('tDelta', bicycle.inputs));
+A(1:mA, index('tDelta', states)) = bicycle.B(:, index('tDelta', ...
+    bicycle.InputName));
 
 % put the neuro block state space in the bottom right corner
-zeta = 0.707; % damping ratio of the neuromuscular mode
-A(mA + 1:end, nA + 1:end) = [0, 1; -neuro^2, -2 * zeta * neuro];
+wnm = neuro(1); % the natural frequency of the neuromuscular mode
+zetanm = neuro(2); % damping ratio of the neuromuscular mode
+%zeta = 0.707; % damping ratio of the neuromuscular mode
+A(mA + 1:end, nA + 1:end) = [0, 1; -wnm^2, -2 * zetanm * wnm];
 
 % build a bicycle C matrix that is minimal for the feedback loop
-minBicycleOutputs = {'psi', 'phi', 'delta', 'phiDot', 'yQ'};
-minBicycleStates = {'yP', 'psi', 'phi', 'delta', 'phiDot', 'deltaDot'};
+if strcmp(stype, 'lateral')
+    minBicycleOutputs = {'psi', 'phi', 'delta', 'phiDot', 'yQ'};
+    minBicycleStates = {'yP', 'psi', 'phi', 'delta', 'phiDot', 'deltaDot'};
+elseif strcmp(stype, 'heading')
+    minBicycleOutputs = {'psi', 'phi', 'delta', 'phiDot'};
+    minBicycleStates = {'psi', 'phi', 'delta', 'phiDot', 'deltaDot'};
+end
 % copy the bicycle C matrix
 minC = bicycle.C;
 % delete the rows and columns for the extra states and outputs
-rows = find(~ismember(bicycle.outputs, minBicycleOutputs));
-cols = find(~ismember(bicycle.states, minBicycleStates));
+rows = find(~ismember(bicycle.OutputName, minBicycleOutputs));
+cols = find(~ismember(bicycle.StateName, minBicycleStates));
 minC(rows, :) = [];
 minC(:, cols) = [];
 
@@ -79,66 +99,75 @@ kDelta = gains(1);
 kPhiDot = gains(2);
 kPhi = gains(3);
 kPsi = gains(4);
-kYQ = gains(5);
 for i = 1:length(minBicycleStates)
-    A(tDeltaDotRow, index(minBicycleStates{i}, states)) = ...
-    -kDelta * neuro^2 * ...
+    coef = ...
+    -kDelta * wnm^2 * ...
     (minC(1, i) * kPhi * kPhiDot * kPsi +...
      minC(2, i) * kPhi * kPhiDot + ...
      minC(3, i) + ...
-     minC(4, i) * kPhiDot + ...
-     minC(5, i) * kPhi * kPhiDot * kPsi * kYQ);
+     minC(4, i) * kPhiDot);
+    if strcmp(stype, 'lateral')
+        kYQ = gains(5);
+        coef = coef + -kDelta * wnm^2 * minC(5, i) * kPhi * kPhiDot * kPsi * kYQ;
+    end
+    A(tDeltaDotRow, index(minBicycleStates{i}, states)) = coef;
 end
 
 % build the input matrix
-% remove tDelta as an input, add yc as the last input to the system and add
-% two rows for the two new state equations
-[mB, nB] = size(bicycle.B);
+% remove tDelta as an input, add yQc or yPsic as the last input to the
+% system and add two rows for the two new state equations
+[mB, ~] = size(bicycle.B);
 B = zeros(mB + 2, length(inputs));
-% for each input that is in bicycle.inputs that isn't steer torque, add the
-% column into B
-% if yc is inputs, add it's column
+% for each input that is in bicycle.InputName that isn't steer torque, add
+% the column into B
+% if yQc or psic is inputs, add it's column
 for i = 1:length(inputs)
-    if strcmp(inputs{i}, 'yc')
-        B(:, i) = [zeros(mB + 1, 1); neuro^2 * prod(gains)];
+    if strcmp(inputs{i}, 'yQc') && strcmp(stype, 'lateral')
+        B(:, i) = [zeros(mB + 1, 1); wnm^2 * prod(gains)];
+    elseif strcmp(inputs{i}, 'psic') && strcmp(stype, 'heading')
+        B(:, i) = [zeros(mB + 1, 1); wnm^2 * prod(gains(1:4))];
     else
-        B(1:mB, i) = bicycle.B(:, index(inputs{i}, bicycle.inputs));
+        B(1:mB, i) = bicycle.B(:, index(inputs{i}, bicycle.InputName));
     end
 end
 
-% the steer torque delta dot equation yc coefficient
-% put the other bicycle inputs other than the steer torque into the new B
-% matrix
-
 % build the output matrix
-[mC, nC] = size(bicycle.C);
+[~, nC] = size(bicycle.C);
 C = zeros(length(outputs), nA + 2);
 for i = 1:length(outputs)
     if strcmp(outputs{i}, 'tDelta')
-        row = find(strcmp(outputs, 'tDelta'));
-        col = find(strcmp(states, 'tDelta'));
+        row = strcmp(outputs, 'tDelta');
+        col = strcmp(states, 'tDelta');
         C(row, col) = 1;
     elseif strcmp(outputs{i}, 'tDeltaDot')
-        row = find(strcmp(outputs, 'tDeltaDot'));
-        col = find(strcmp(states, 'tDeltaDot'));
+        row = strcmp(outputs, 'tDeltaDot');
+        col = strcmp(states, 'tDeltaDot');
         C(row, col) = 1;
     else
         % grab the row in the bicycle output matrix and put it in the system
         % output matrix
-        C(i, 1:nC) = bicycle.C(find(strcmp(bicycle.outputs, outputs{i})), :);
+        C(i, 1:nC) = bicycle.C(find(strcmp(bicycle.OutputName, outputs{i})), :);
     end
 end
 
 % build the feed forward matrix, this is always zero
 D = zeros(length(outputs), length(inputs));
 
-sys.A = A;
-sys.B = B;
-sys.C = C;
-sys.D = D;
-sys.states = states;
-sys.inputs = inputs;
-sys.outputs = outputs;
+sys = ss(A, B, C, D);
+sys.StateName = states;
+sys.InputName = inputs;
+sys.OutputName = outputs;
+if strcmp(stype, 'lateral')
+    sys.Name = 'Lateral Deviation Tracking';
+elseif strcmp(stype, 'heading')
+    sys.Name = 'Heading Tracking';
+end
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Sub-Functions
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% this function is probably unecessary, as logical indexing can be use
+% C(strcmp(value, vector), :) for example
 function i = index(value, vector)
-    i = find(strcmp(value, vector));
+i = find(strcmp(value, vector));
