846644b949ad9bb4a8ea57ffc898364cf4124579
Jason K. Moore <moorepants@gmail.com>
2016-01-07 14:29:47 -0800

    Added all bikes to the handling quality vs speed plot.

--- handling_vs_speed.m
+++ handling_vs_speed.m
@@ -1,16 +1,20 @@
 speeds = linspace(2, 10, 20);
 freqs = linspace(0.01, 20, 100);
 
-bicycle = 'Benchmark'
-
-peaks = zeros(length(speeds));
-
-for i = 1:length(speeds)
-    data = generate_data(bicycle, speeds(i));
-    num = data.handlingMetric.num;
-    den = data.handlingMetric.den;
-    [mag, ~, ~] = bode(tf(num, den), freqs);
-    peaks[i] = max(mag);
+bikes = {'Benchmark', 'Browserins', 'Browser', 'Pista', ...
+         'Fisher', 'Yellow', 'Yellowrev'};
+hold on;
+for i = 1:length(bikes)
+    peaks = zeros(length(speeds));
+    for j = 1:length(speeds)
+        data = generate_data(bikes{i}, speeds(j));
+        num = data.handlingMetric.num;
+        den = data.handlingMetric.den;
+        [mag, ~, ~] = bode(tf(num, den), freqs);
+        peaks(j) = max(mag);
+    end
+    plot(speeds, peaks)
 end
-
-plot(speeds, peaks)
+plot(speeds, ones(size(speeds)) * 5);
+plot(speeds, ones(size(speeds)) * 8);
+hold off;
