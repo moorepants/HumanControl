6d772677bdf9655bd9ac6f26ca46673fde3a7f96
Jason K. Moore <moorepants@gmail.com>
2016-08-04 17:36:14 +0700

    Stop plotting zeros and add some labels.

--- handling_vs_par.m
+++ handling_vs_par.m
@@ -1,11 +1,11 @@
 
-w = linspace(pi / 20, 3 * pi / 20, 19);
-peaks = zeros(length(w));
+w = linspace(0.1, 1.1, 30);
+peaks = zeros(1, length(w));
 freqs = linspace(0.01, 20, 100);
 
 for j = 1:length(w)
     par = par_text_to_struct('parameters/BenchmarkPar.txt');
-    par.lam = w(j);
+    par.w = w(j);
     [A, B, C, D] = whipple_pull_force_abcd(par, 5.0);
     data = generate_data('Benchmark', 5.0, ...
                          'simulate', false, ...
@@ -20,3 +20,5 @@ for j = 1:length(w)
 end
 
 plot(w, peaks)
+xlabel('Wheelbase')
+ylabel('Max of HQM')
