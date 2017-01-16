6f2eadc79355bce87ecf1d596758fb192d420cfb
Jason K. Moore <moorepants@gmail.com>
2017-01-16 11:47:53 -0800

    Increased freq range in objective changed stablity check to be more robust.

--- objective.m
+++ objective.m
@@ -1,29 +1,36 @@
 function [peak_hqm] = objective(unknowns)
 
+display('==========================================')
+
 unknowns
 
-freqs = linspace(0.01, 20, 100);
+freqs = linspace(0.01, 40, 200);
 
 par = par_text_to_struct('parameters/BenchmarkPar.txt');
 
+%par.g = 1.6;  % acceleration due to gravity on the moon
+%par.g = 3.71;  % acceleration due to gravity on mars
 par.c = unknowns(1);
 par.w = unknowns(2);
 par.lam = unknowns(3);
 par.rF = unknowns(4);
 
-[A, B, C, D] = whipple_pull_force_abcd(par, 5.0);
+speed = 5.0;
 
-data = generate_data('Benchmark', 5.0, ...
+[A, B, C, D] = whipple_pull_force_abcd(par, speed);
+
+data = generate_data('Benchmark', speed, ...
                      'simulate', false, ...
                      'forceTransfer', {}, ...
                      'fullSystem', false, ...
                      'stateSpace', {A, B, C, D}, ...
                      'display', 0);
 
-roll_loop = minreal(tf(data.closedLoops.Phi.num, data.closedLoops.Phi.den));
+%roll_loop = minreal(tf(data.closedLoops.Phi.num, data.closedLoops.Phi.den));
+lateral_dev_loop = minreal(tf(data.closedLoops.Y.num, data.closedLoops.Y.den));
 
-if ~isstable(roll_loop)
-    peak_hqm = 100;
+if ~isstable(lateral_dev_loop)
+    peak_hqm = max(10, 100 * max(real(pole(lateral_dev_loop))));
 else
     num = data.handlingMetric.num;
     den = data.handlingMetric.den;
