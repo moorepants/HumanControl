86d446a6c46c5c89ab645cf0853907f8213f1520
Jason K. Moore <moorepants@gmail.com>
2017-01-16 11:43:08 -0800

    Added wheelbase constraint and specific sigmas.

--- optimal_bicycle.m
+++ optimal_bicycle.m
@@ -6,7 +6,13 @@ guess(2) = par.w;
 guess(3) = par.lam;
 guess(4) = par.rF;
 
-opts.LBounds = [-inf; 0.010; -pi/2; 1e-10];
+% wheelbase should always accomdate the mass center
+min_wheelbase = (par.mH * par.xH + par.mB * par.xB) / (par.mH + par.mB);
+
+opts.LBounds = [-inf; min_wheelbase; -pi/2; 1e-10];
 opts.UBounds = [inf; inf; pi/2; inf];
 
-[optimal_par, hqm] = cmaes('objective', guess, sqrt(var(guess')'), opts);
+sigma = [0.5; 3.0; 0.3 * pi; 0.2];
+%sigma = sqrt(var(guess')');
+
+[optimal_par, hqm] = cmaes('objective', guess, sigma, opts);
