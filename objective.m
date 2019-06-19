82ea021a249356412d48ec104c9af388a08b82ed
Jason K. Moore <moorepants@gmail.com>
2019-06-19 13:15:17 -0700

    Switched to finding the optimal front wheel inertia and restricting to physically realizable wheels.

--- objective.m
+++ objective.m
@@ -1,21 +1,58 @@
 function [peak_hqm] = objective(unknowns)
+% OBJECTIVE - Returns the maximum value of the HQM transfer function
+% magnitude if the system is closed loop stable or a large value based on
+% how large the closed loop poles are if the system is closed loop unstable.
+%
+% Syntax: peak_hqm = objective(unknowns)
+%
+% Inputs:
+%   unknowns - Vector of the optimization parameters (c trail, w wheelbase,
+%              lam steer axis tilt, IFyy front wheel rotational inertia),
+%              size 4x1.
+% Outputs:
+%   peak_hqm - Maximum scalar value of the HQM magnitude.
+
+% NOTE : This value has to be manually set to be the target speed for the
+% optimal design.
+% TODO : Move setting this into "optimal_bicycle.m".
+speed = 5.0;
 
 display('==========================================')
 
+display('Values passed into the objective function:')
 unknowns
 
 freqs = linspace(0.01, 40, 200);
 
+% NOTE : This has to be manually set and match that in "optimal_bicycle.m".
 par = par_text_to_struct('parameters/BenchmarkPar.txt');
 
+% NOTE : Here to try for fun to see if different bicycle designs are needed
+% for riding in low gravity.
 %par.g = 1.6;  % acceleration due to gravity on the moon
 %par.g = 3.71;  % acceleration due to gravity on mars
-par.c = unknowns(1);
-par.w = unknowns(2);
-par.lam = unknowns(3);
-par.rF = unknowns(4);
 
-speed = 5.0;
+par.c = unknowns(1);  % trail
+par.w = unknowns(2);  % wheelbase
+par.lam = unknowns(3); % steer axis tilt
+
+% Given an existing wheel with a rotational inertia, mass, and radius that
+% approximately adhere to I=m*r^2, make sure that this relationship holds
+% for the optimal front wheel inertias and that we only have to add mass to
+% the existing wheel while keeping the radius constant or reduce the radius
+% while keeping mass constant. These should be physically realizable barring
+% that the radius doesn't get too tiny.
+IFyy_opt = unknowns(4);
+
+if IFyy_opt < par.IFyy
+    % keep the mass the same but reduce the wheel radius
+    par.rF = sqrt(IFyy_opt ./ par.mF);
+else IFyy_opt > par.IFyy
+    % keep radius of the wheel the same but add mass
+    par.mF = IFyy_opt / par.rF.^2;
+end
+
+par.IFyy = IFyy_opt; % front wheel rotational inertia
 
 [A, B, C, D] = whipple_pull_force_abcd(par, speed);
 
@@ -30,6 +67,10 @@ data = generate_data('Benchmark', speed, ...
 lateral_dev_loop = minreal(tf(data.closedLoops.Y.num, data.closedLoops.Y.den));
 
 if ~isstable(lateral_dev_loop)
+    % TODO : It may be best to return NAN here as per the CMAES statement
+    % "An easy way to implement a hard non-linear constraint is to return
+    % NaN. Then, this function evaluation is not counted and a newly sampled
+    % point is tried immediately."
     peak_hqm = max(10, 100 * max(real(pole(lateral_dev_loop))));
 else
     num = data.handlingMetric.num;
@@ -38,4 +79,5 @@ else
     peak_hqm = max(mag);
 end
 
+display('Value of the objective function:')
 peak_hqm
--- optimal_bicycle.m
+++ optimal_bicycle.m
@@ -4,15 +4,25 @@ guess = zeros(4, 1);
 guess(1) = par.c;
 guess(2) = par.w;
 guess(3) = par.lam;
-guess(4) = par.rF;
+guess(4) = par.IFyy;
 
 % wheelbase should always accomdate the mass center
 min_wheelbase = (par.mH * par.xH + par.mB * par.xB) / (par.mH + par.mB);
 
-opts.LBounds = [-inf; min_wheelbase; -pi/2; 1e-10];
-opts.UBounds = [inf; inf; pi/2; inf];
+opts.LBounds = [-inf;          % c
+                min_wheelbase; % w
+                -pi/2;         % lam
+                0.0];          % IFyy
 
-sigma = [0.5; 3.0; 0.3 * pi; 0.2];
+opts.UBounds = [inf;  % c
+                inf;  % w
+                pi/2; % lam
+                inf]; % IFyy
+
+sigma = [0.5;
+         3.0;
+         0.3 * pi;
+         0.07];
 %sigma = sqrt(var(guess')');
 
 [optimal_par, hqm] = cmaes('objective', guess, sigma, opts);
