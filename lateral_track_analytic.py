ed744e3b191159651d388ccb5a3c8ed55f164d7c
Jason K. Moore <moorepants@gmail.com>
2025-11-20 06:42:51 +0100

    Added a simple Python simulation of the model.

--- lateral_track_analytic.py
+++ lateral_track_analytic.py
@@ -4,12 +4,29 @@ from sympy import Symbol, symbols, Matrix, zeros
 
 # build the bicycle state space
 b = {}
-delta, deltaDot, phiDot, phi, psi, yP, yQ =\
-    symbols('delta, deltaDot, phiDot, phi, psi, yP, yQ')
+delta, deltaDot, phiDot, phi, psi, yP, yQ = symbols(
+    'delta, deltaDot, phiDot, phi, psi, yP, yQ')
 tPhi, tDelta, fB, tDeltaDot = symbols('tPhi, tDelta, fB, tDeltaDot')
-b['x'] = Matrix([[yP], [psi], [phi], [delta], [phiDot], [deltaDot]])
-b['u'] = Matrix([[tPhi], [tDelta], [fB]])
-b['y'] = Matrix([[psi], [phi], [delta], [phiDot], [yQ]])
+b['x'] = Matrix([
+    [phi],
+    [delta],
+    [phiDot],
+    [deltaDot],
+    [psi],
+    [yP],
+])
+b['u'] = Matrix([
+    [tPhi],
+    [tDelta],
+    [fB],
+])
+b['y'] = Matrix([
+    [phi],
+    [delta],
+    [phiDot],
+    [psi],
+    [yQ],
+])
 
 numStates = len(b['x'])
 numInputs = len(b['u'])
@@ -20,25 +37,36 @@ numOutputs = len(b['y'])
 # because we are tracking the lateral deviation of the front wheel contact. In
 # this case, set the appropriate entries that are ones and zeros so that the
 # final equatios will be in a reasonably understandable form.
-b['A'] = Matrix(numStates, numStates,
-        lambda i, j: Symbol('a' + str(i + 1) + str(j + 1)))
+b['A'] = zeros(numStates, numStates)
+b['A'][0, 2] = 1
+b['A'][1, 3] = 1
+b['A'][2, 0] = Symbol('a_phidd_phi')
+b['A'][2, 1] = Symbol('a_phidd_del')
+b['A'][2, 2] = Symbol('a_phidd_phid')
+b['A'][2, 3] = Symbol('a_phidd_deld')
+b['A'][3, 0] = Symbol('a_deldd_phi')
+b['A'][3, 1] = Symbol('a_deldd_del')
+b['A'][3, 2] = Symbol('a_deldd_phid')
+b['A'][3, 3] = Symbol('a_deldd_deld')
+b['A'][4, 1] = Symbol('a_psid_del')
+b['A'][4, 3] = Symbol('a_psid_deld')
+b['A'][5, 4] = Symbol('a_ypd_psi')
 
-b['B'] = Matrix(numStates, numInputs,
-        lambda i, j: Symbol('b' + str(i + 1) + str(j + 1)))
+b['B'] = zeros(numStates, numInputs)
+b['B'][2, 1] = Symbol('b_phidd_Tdel')
+b['B'][2, 2] = Symbol('b_phidd_F')
+b['B'][3, 1] = Symbol('b_deldd_Tdel')
+b['B'][3, 2] = Symbol('b_deldd_F')
 
-#b['C'] = Matrix(numOutputs, numStates,
-        #lambda i, j: Symbol('c' + str(i + 1) + str(j + 1)))
-
-b['C'] = zeros((numOutputs, numStates))
-b['C'][0, 1] = 1 # psi
-b['C'][1, 2] = 1 # phi
-b['C'][2, 3] = 1 # delta
-b['C'][3, 4] = 1 # phiDot
+b['C'] = zeros(numOutputs, numStates)
+b['C'][0, 0] = 1  # phi
+b['C'][1, 1] = 1  # delta
+b['C'][2, 2] = 1  # phiDot
+b['C'][3, 4] = 1  # psi
 # yQ depends on the rear wheel location, the heading angle and the steer angle
-b['C'][4, 0] = 1
-c52, c54 = symbols('c52 c54')
-b['C'][4, 1] = c52
-b['C'][4, 3] = c54
+b['C'][4, 1] = Symbol('c_yq_del')
+b['C'][4, 4] = Symbol('c_yq_psi')
+b['C'][4, 5] = 1
 
 b['D'] = Matrix(numOutputs, numInputs, lambda i, j: 0.)
 
@@ -52,15 +80,21 @@ phiDotc = kPhi * (phic - phi)
 deltac = kPhiDot * (phiDotc - phiDot)
 Unm = kDelta * (deltac - delta)
 Unm = Unm.expand()
-controller = Matrix([Unm.coeff(psi), Unm.coeff(phi), Unm.coeff(delta),
-    Unm.coeff(phiDot), Unm.coeff(yQ), Unm.coeff(yc)]).T
+controller = Matrix([
+    Unm.coeff(phi),
+    Unm.coeff(delta),
+    Unm.coeff(phiDot),
+    Unm.coeff(psi),
+    Unm.coeff(yQ),
+    Unm.coeff(yc),
+]).T
 
 # neuromuscular state space
-omega = Symbol('w')
+omega = Symbol('omega')
 zeta = Symbol('zeta')
 n = {}
 n['A'] = Matrix([[0, 1],
-                 [-omega**2, -2 * zeta * omega]])
+                 [-omega**2, -2*zeta*omega]])
 n['B'] = Matrix([[0],
                  [omega**2]])
 n['C'] = Matrix([1, 0]).T
@@ -71,14 +105,17 @@ n['u'] = Matrix([Unm])
 n['y'] = Matrix([tDelta])
 
 # Compute the differential equations for the closed loop system.
-xDot = b['A'] * b['x'] + b['B'] * b['u']
-xDot = xDot.col_join(n['A'] * n['x'] + n['B'] * controller * (b['C'] *
-        b['x']).col_join(Matrix([yc])))
+xDot = b['A']*b['x'] + b['B']*b['u']
+xDot = xDot.col_join(
+    n['A']*n['x'] + n['B']*controller*(b['C']*b['x']).col_join(
+        Matrix([yc])))
+y = (b['C']*b['x']).col_join(n['C']*n['x'])
 
 # Build the closed loop state space matrices.
-systemStates = [yP, psi, phi, delta, phiDot, deltaDot, tDelta, tDeltaDot]
-systemInputs = [tPhi, fB, yc]
-systemOutputs = [psi, phi, delta, phiDot, yQ, tDelta]
+systemStates = [phi, delta, phiDot, deltaDot, psi, yP, tDelta, tDeltaDot]
+systemInputs = [fB, yc]
+systemOutputs = [phi, delta, phiDot, psi, yQ, tDelta]
+
 
 def mat_coeff(equations, variables, i, j):
     c = equations[i].expand().coeff(variables[j])
@@ -86,13 +123,12 @@ def mat_coeff(equations, variables, i, j):
         c = 0
     return c
 
-A = Matrix(len(systemStates), len(systemStates), lambda i, j: mat_coeff(xDot,
-    systemStates, i, j))
 
-B = Matrix(len(systemStates), len(systemInputs), lambda i, j: mat_coeff(xDot,
-    systemInputs, i, j))
+A = Matrix(len(systemStates), len(systemStates),
+           lambda i, j: mat_coeff(xDot, systemStates, i, j))
 
-y = (b['C'] * b['x']).col_join(n['C'] * n['x'])
+B = Matrix(len(systemStates), len(systemInputs),
+           lambda i, j: mat_coeff(xDot, systemInputs, i, j))
 
-C = Matrix(len(systemOutputs), len(systemStates), lambda i, j: mat_coeff(y,
-    systemStates, i, j))
+C = Matrix(len(systemOutputs), len(systemStates),
+           lambda i, j: mat_coeff(y, systemStates, i, j))
---
+++ simulate_controlled_system.py
@@ -0,0 +1,76 @@
+import sympy as sm
+import numpy as np
+from scipy.signal import lsim, lti
+import matplotlib.pyplot as plt
+
+from lateral_track_analytic import (A, B, C, systemOutputs)
+
+args = list(sm.ordered(list(A.free_symbols | B.free_symbols | C.free_symbols)))
+
+eval_sys = sm.lambdify(args, (A, B, C))
+
+# q'' = Minv(-(g*K0 + v**2*K2)*q - v*C1*q' + T + H*F))
+# Bicycle 1 from Hess, Moore, Hubbard 2012
+M = np.array([[106.0, 1.55], [1.55, 0.25]])
+K0 = np.array([[-93.2, -1.76], [-1.76, -0.68]])
+K2 = np.array([[0.0, 77.3], [0.0, 1.58]])
+C1 = np.array([[0.0, 29.9], [-0.45, 1.08]])
+v, g = 5.0, 9.81
+c, w, lam = 0.068, 1.12, 0.4
+# H from Jason in Moore 2012 (should be Bicycle 1 values to be perfectly
+# correct)
+H = np.array([[0.943], [0.011]])
+# Bicycle plant state space
+Minv = np.linalg.inv(M)
+# x = phi, del, phid, deld
+Ab = np.block([
+    [np.zeros_like(M), np.eye(2)],
+    [-Minv@(g*K0 + v**2*K2), -Minv*v@C1]
+])
+# u = Tphi, Tdel, F
+Bb = np.block([
+    [np.zeros_like(M), np.zeros((2, 1))],
+    [Minv, Minv@H]
+])
+
+vals = np.array([
+    Ab[3, 1],  # a_deldd_del,
+    Ab[3, 3],  # a_deldd_deld,
+    Ab[3, 0],  # a_deldd_phi,
+    Ab[3, 2],  # a_deldd_phid,
+    Ab[2, 1],  # a_phidd_del,
+    Ab[2, 3],  # a_phidd_deld,
+    Ab[2, 0],  # a_phidd_phi,
+    Ab[2, 2],  # a_phidd_phid,
+    v/w*np.cos(lam),  # a_psid_del,
+    c/w*np.cos(lam),  # a_psid_deld,
+    v,  # a_ypd_psi,
+    Bb[3, 2],  # b_deldd_F,
+    Bb[3, 1],  # b_deldd_Tdel,
+    Bb[2, 2],  # b_phidd_F,
+    Bb[2, 1],  # b_phidd_Tdel,
+    -c*np.cos(lam),  # c_yq_del,
+    w,  # c_yq_psi,
+    48.0,  # kDelta,
+    9.03,  # kPhi,
+    -0.08,  # kPhiDot,
+    0.161,  # kPsi,
+    0.097,  # kYQ,
+    30.0,  # omega
+    0.707,  # zeta
+])
+
+A_cl, B_cl, C_cl = eval_sys(*vals)
+
+sys = lti(A_cl, B_cl, C_cl, np.zeros((C_cl.shape[0], B_cl.shape[1])))
+t = np.linspace(0.0, 5.0, num=100)
+u = np.zeros((len(t), B_cl.shape[1]))
+u[:, 1] = 0.2
+
+t, y, x = lsim(sys, u, t)
+
+fig, axes = plt.subplots(nrows=y.shape[1])
+for yi, ax, lab in zip(y.T, axes, systemOutputs):
+    ax.plot(t, yi, label=lab)
+    ax.legend()
+plt.show()
